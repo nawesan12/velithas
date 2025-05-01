760afef87a735a92deeb6f8d132ac436f88d2599
Nahuel <nahueltecnicomdp@gmail.com>
2025-05-01 01:03:36 -0300

    a

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE =
+  "Velithas | Velas de Soja | Ilumina tu ambiente. Encende tu llama.";
+export const SITE_DESCRIPTION =
+  "Ilumina tu ambiente. Encende tu llama. Estamos comprometidos con darte el mejor servicio de velas aromáticas.";
